71cd378c1cadadcf17e2fe29adf5559de075654f
Changbum97 <chb2005@naver.com>
2023-01-01 19:09:57 +0900

    First Commit

---
+++ src/main/java/study/loginstudy/LoginStudyApplication.java
@@ -0,0 +1,13 @@
+package study.loginstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/loginstudy/LoginStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.loginstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
