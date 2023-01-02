f5dab8225e2717b7202251b09502bb7a56684bbe
Changbum97 <chb2005@naver.com>
2023-01-02 18:18:19 +0900

    FEAT: UserRepository 구현

--- src/main/java/study/loginstudy/repository/UserRepository.java
+++ src/main/java/study/loginstudy/repository/UserRepository.java
@@ -1,2 +1,12 @@
-package study.loginstudy.repository;public interface UserRepository {
+package study.loginstudy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.loginstudy.domain.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByLoginId(String loginId);
+    boolean existsByNickname(String nickname);
+    Optional<User> findByLoginId(String loginId);
 }
