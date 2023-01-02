abf147a91728433bd78ea51b8319ef2f48bdd4a9
Changbum97 <chb2005@naver.com>
2023-01-02 18:17:45 +0900

    FEAT: 데이터 초기화 코드 구현

--- src/main/java/study/loginstudy/MakeInitData.java
+++ src/main/java/study/loginstudy/MakeInitData.java
@@ -1,2 +1,35 @@
-package study.loginstudy;public class MakeInitData {
+package study.loginstudy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import study.loginstudy.domain.UserRole;
+import study.loginstudy.domain.entity.User;
+import study.loginstudy.repository.UserRepository;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class MakeInitData {
+
+    private final UserRepository userRepository;
+
+    @PostConstruct
+    public void makeAdminAndUser() {
+        User admin = User.builder()
+                .loginId("admin")
+                .password("1234")
+                .nickname("관리자")
+                .role(UserRole.ADMIN)
+                .build();
+        userRepository.save(admin);
+
+        User user = User.builder()
+                .loginId("user")
+                .password("1234")
+                .nickname("User1")
+                .role(UserRole.USER)
+                .build();
+        userRepository.save(user);
+    }
 }
